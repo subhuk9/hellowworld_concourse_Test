36d585f26ec136a40975b8b76bf530025dff5b3f
Subbu <subramanyam_kotra@comcast.com>
2021-10-07 13:09:08 -0400

    updated ci config

---
+++ ci/scripts/install-and-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+apt-get update
+apt-get -y install gconf-service libasound2 libatk1.0-0 libcairo2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+echo "Node Version: $(node --version)"
+echo "NPM Version: $(npm --version)"
+
+cd code
+
+npm install
+npm run test
